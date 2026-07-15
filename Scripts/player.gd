5390e4d42a6ffdbdaa21b5288ed01a91afa0156d
Filippo Soffiatti <filippo.soff05@gmail.com>
2026-07-15 17:23:11 +0200

    Updating script

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,101 +1,110 @@
 extends CharacterBody3D
 
-var speed = 8.0 #Player speed
-var gravity = 9.81 #Gravity
-var senstivity = 0.008 #Mouse senstivity
-var is_sprinting := false #Sprinting flag
-var jump_buffer_time = 0.15  #Seconds of tollerance
-var jump_buffer_timer = 0.0  #Internal timer
-var crouch_toggle := false; #Crouch toggle
+#SPEED VARIABLES
+var base_speed = 8.0 #Base player speed
+var speed = base_speed #Current speed (Default to base_speed)
+var sprint_speed = 14.0 #Sprint player speed
+var shift_speed = 4.0 #Shift player speed
+var crouch_speed = 2.0 #Crouch player speed
 
-@onready var head = $Head #Head reference (Camera pivot)
+#TOGGLES
+var is_sprinting := false #Sprint toggle
+var is_crouching := false #Crouch toggle
+
+#JUMP BUFFER
+var jump_buffer_time = 0.15 #
+var jump_buffer_timer = 0.0
+
+#OTHERS
+var gravity = 9.81
+var senstivity = 0.006;
+
+@onready var head = $Head #Head reference
 @onready var camera = $Head/Camera3D #Camera reference
 
 var player_velocity = Vector3.ZERO #Empty vector of the player velocity
 
 func _ready(): #Called when the player is ready on the scene
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	 #Function to block mouse movement
+	#Function that blocks the mouse movement
 	
-func _unhandled_input(event): #Used for unhandled inputs
+func _unhandled_input(event): #Used for the unhandled inputs
 	
 	if event is InputEventMouseMotion: #If there is mouse movement
 		head.rotate_y(-event.relative.x * senstivity) #Rotate head
 		camera.rotate_x(-event.relative.y * senstivity / 1.5) #Rotate camera
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 		#Set a clamp to the max rotation upwards and downwards of the camera
-
-func _process(delta): #Process function (Relative to the current fps)
-	
-	if is_on_floor() and Input.is_action_just_pressed("Crouch"): 
-		crouch_toggle = !crouch_toggle #When pressing "crouch" the state of the variabile changes to true or false
 		
-	if crouch_toggle == true: #While croch_toggle is true the player stays in "crouch" and it's slower
-		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 0.4, delta * 10.0)
-		speed = lerp(speed, 3.0, 0.05)	
-	else:
-		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.0, delta * 10.0)
-
-func _physics_process(delta): #Phyisics function (locked to 60fps)
+func _get_target_speed(): #Used for returning the needed speed based on the current action
+	
+	if is_crouching == true:
+		return crouch_speed
+	if Input.is_action_pressed("Shift"):
+		return shift_speed
+	if is_sprinting == true:
+		return sprint_speed
+	return base_speed
+		
+func _physics_process(delta): #Used for physics related things (Locked at 60fps)
 	
 	#WASD
 	var player_direction = Vector3.ZERO #Empty vector of the player direction
 	
-	var inputs = Input.get_vector("Left", "Right", "Forward", "Backward") #A 2d vector that contains the movement inputs
+	var inputs = Input.get_vector("Left", "Right", "Forward", "Backward")
+	#A 2 vector that return inputs.x based of the first two inputs and input.y based on the second two
 	
-	player_direction = (head.transform.basis * Vector3(inputs.x, 0, inputs.y)) #Assigns the inputs vector to the player_direction vector based on the head direction
-			
+	player_direction = (head.transform.basis * Vector3(inputs.x, 0, inputs.y))
+	#Assigns the inputs vector to the player_direction vector based on the head direction
+ 
 	#SPRINTING
 	if Input.is_action_pressed("Sprint"): #Checking if the player is sprinting
-		is_sprinting = true
+		is_sprinting = true #Sets is_sprinting true
 	else:
-		is_sprinting = false
+		is_sprinting = false #Sets is_sprinting false
+		
+	#CROUCHING
+	if is_on_floor() and Input.is_action_just_pressed("Crouch"):
+		is_crouching = !is_crouching #When pressing "crouch" the state of the variable is inverted
+ 
+	if is_crouching == true: #While crouch_toggle is true the player stays in crouch
+		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 0.5, delta * 10.0)
+	else:
+		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.0, delta * 10.0)
 	
-	if is_on_floor(): #If the player is sprinting on the floor it sprints gradually with lerp()
-		if is_sprinting == true:
-			speed = lerp(speed, 14.0, 0.05)	
-		else: #If not he doesn't
-			speed = lerp(speed, 8.0, 0.1)
-			
-	#SHIFTING
-	if Input.is_action_pressed("Shift"): #The player goes into "shift" gradually
-			speed = lerp(speed, 4.0, 0.5)
-	if Input.is_action_just_released("Shift"): #the player exits "shift" gradually
-			speed = lerp(speed, 8.0, 0.3)
+	#SPEED 
+	var target_speed = _get_target_speed()
+	speed = lerp(speed, target_speed, 0.1)
 	
 	#GRAVITY
 	if is_on_floor(): #When is on floor the player_velocity.y is set to 0
 		player_velocity.y = 0
 	else: #When is not on floor the player_velocity.y is changed
-		if player_velocity.y < 0:
-			player_velocity.y -= (gravity * delta) 
-			#When the player jumps, while he goes up the gravity is normal
+		if player_velocity.y > 0:
+			player_velocity.y -= (gravity * delta * 1.5)
+			#While the player is going up (velocity.y positive) gravity is normal
 		else:
-			player_velocity.y -= (gravity * delta * 2.5) 
-			#When the player is falling the gravity is increased
-			
+			player_velocity.y -= (gravity * delta * 1.8)
+			#While the player is falling (velocity.y negative or zero) gravity is increased
+	
 	#JUMPING
 	if jump_buffer_timer > 0: #If the buffer_timer is greater than 0 it begins to reduce every delta frame
 		jump_buffer_timer -= delta
-		
+ 
 	if Input.is_action_just_pressed("Jump"): #If the player jumps the buffer time is added to the timer
 		jump_buffer_timer = jump_buffer_time
-	
+ 
 	if is_on_floor() and jump_buffer_timer > 0: #If the player is on the floor and the buffer is not 0 the player jumps
 		player_velocity.y = 6
 		jump_buffer_timer = 0.0
-			
+	
 	#MOVEMENT
 	if is_on_floor(): #If the player is on the floor the player velocity is interpolated beetween the player direction times the speed and the velocity of the previous frame
 		player_velocity.x = lerp(player_velocity.x, player_direction.x * speed, 0.2)
 		player_velocity.z = lerp(player_velocity.z, player_direction.z * speed, 0.2)
-		
 	else: #If the player is not on the floor the player can only change the direction a little
 		player_velocity.x = lerp(player_velocity.x, player_direction.x * speed, 0.03)
 		player_velocity.z = lerp(player_velocity.z, player_direction.z * speed, 0.03)
-		
+ 
 	velocity = player_velocity #Define where the player goes based on the player velocity
 	move_and_slide() #Makes the player move
-		
-	
-	
