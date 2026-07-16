7949542462493054c91bc2373870a9b547501000
Filippo Soffiatti <filippo.soff05@gmail.com>
2026-07-16 17:23:44 +0200

    Adding head bob

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,23 +1,34 @@
 extends CharacterBody3D
 
 #SPEED VARIABLES
-var base_speed = 8.0 #Base player speed
+var base_speed = 3.0 #Base player speed
 var speed = base_speed #Current speed (Default to base_speed)
-var sprint_speed = 14.0 #Sprint player speed
-var shift_speed = 4.0 #Shift player speed
-var crouch_speed = 2.0 #Crouch player speed
+var sprint_speed = 6.0 #Sprint player speed
+var shift_speed = 2.0 #Shift player speed
+var crouch_speed = 1.0 #Crouch player speed
 
 #TOGGLES
 var is_sprinting := false #Sprint toggle
 var is_crouching := false #Crouch toggle
 
 #JUMP BUFFER
-var jump_buffer_time = 0.15 #
-var jump_buffer_timer = 0.0
+var jump_buffer_time = 0.15 #Jump buffer time
+var jump_buffer_timer = 0.0 #Jump buffer timer
 
 #OTHERS
-var gravity = 9.81
-var senstivity = 0.006;
+var gravity = 9.81 #Gravity
+var senstivity = 0.006 #Mouse senstivity
+
+#HEAD BOB
+@export var bob_frequency = 10.0 #Default head bob frequency
+@export var bob_height = 0.05 #Default head bob "height"
+@export var bob_sprint_frequency_multiplier = 1.3 #Spriting head bob multiplier
+@export var bob_sprint_height_multiplier = 1.1 #Sprinting head bob "height" multiplier
+@export var bob_smooth = 8.0 #Head bob smoothness
+
+var bob_time = 0.0
+var bob_current_intensity = 0.0
+var camera_default_position: Vector3
 
 @onready var head = $Head #Head reference
 @onready var camera = $Head/Camera3D #Camera reference
@@ -27,6 +38,8 @@ var player_velocity = Vector3.ZERO #Empty vector of the player velocity
 func _ready(): #Called when the player is ready on the scene
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	#Function that blocks the mouse movement
+	
+	camera_default_position = camera.position #Sets camera default position when player is ready
 	
 func _unhandled_input(event): #Used for the unhandled inputs
 	
@@ -45,6 +58,36 @@ func _get_target_speed(): #Used for returning the needed speed based on the curr
 	if is_sprinting == true:
 		return sprint_speed
 	return base_speed
+	
+func _update_head_bob(delta): #Used for the head bob
+	
+	#Set a Vector2 to get total speed of the horizontal speed (in any direction)
+	var horizontal_speed = Vector2(player_velocity.x, player_velocity.z).length()
+	
+	#Sets "moving" to true if both conditions are true
+	var moving = horizontal_speed > 0.5 and is_on_floor()
+	
+	#Sets the target_intesity to 1 if it's moving or 0 if not
+	var target_intensity = 1.0 if moving else 0.0
+	
+	#The current intensity is interpolated with the target_intensity (current frame) by the smoothness
+	bob_current_intensity = lerp(bob_current_intensity, target_intensity, bob_smooth * delta)
+	
+	#If the player sprints the frequency and the height of the head bob increase
+	var frequency_multiplier = bob_sprint_frequency_multiplier if is_sprinting else 1.0
+	var height_multiplier = bob_sprint_height_multiplier if is_sprinting else 1.0
+	
+	#If the intensity is greater than 0 gets multpilied by the frequency and the frequency multiplier
+	if bob_current_intensity > 0.001:
+		bob_time += delta * bob_frequency * frequency_multiplier
+	else:
+		bob_time = 0.0
+	
+	#The vertical offset is set based off -1 and 1 of the sin() function times the maxium height with or without the multiplier and by the current intensity
+	var vertical_offset = sin(bob_time) * bob_height * height_multiplier * bob_current_intensity
+	
+	#It changes the vertical camera position
+	camera.position = camera_default_position + Vector3(0, vertical_offset, 0)
 		
 func _physics_process(delta): #Used for physics related things (Locked at 60fps)
 	
@@ -108,3 +151,6 @@ func _physics_process(delta): #Used for physics related things (Locked at 60fps)
  
 	velocity = player_velocity #Define where the player goes based on the player velocity
 	move_and_slide() #Makes the player move
+	
+	#I put it here to get the update velocity parameters of the current frame
+	_update_head_bob(delta)
